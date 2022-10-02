5f0a4934e9ec116cc3d4f74e93f0c5458714cf73
JYoung554 <jontefears10@gmail.com>
2022-10-02 19:01:19 -0400

    Updated

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/artists', controllers.getArtists)
 router.delete('/artists/:id', controllers.deleteArtist)
 
 router.post('/albums', controllers.addAlbum)
-router.get('/albums', controllers.getAlbums)
+router.get('/albums', controllers.getAlbum)
 router.delete('/albums/:id', controllers.deleteAlbum)
 
 module.exports = router
