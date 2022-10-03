c275208d06a18b1ab2fa39d472a70702e0c3c788
JYoung554 <jontefears10@gmail.com>
2022-10-03 14:09:20 -0400

    Updated

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/apollocatalog'
+    : 'mongodb://127.0.0.1:27017/tranquil-reef-21193'
 mongoose
   .connect(dbUrl, {
     useUnifiedTopology: true,
