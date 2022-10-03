91883fe978e5b56b25a8b3374a5cec11c997a499
JYoung554 <jontefears10@gmail.com>
2022-10-03 18:59:37 -0400

    Updated

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const controllers = require('../controllers')
 const router = Router()
 router.get('/', (req, res) => res.send('This is root!'))
 
-router.post('/artists', controllers.addArtist)
-router.get('/artists', controllers.getArtists)
-router.delete('/artists/:id', controllers.deleteArtist)
+router.post('/api/artists', controllers.addArtist)
+router.get('/api/artists', controllers.getArtists)
+router.delete('/api/artists/:id', controllers.deleteArtist)
 
-router.post('/albums', controllers.addAlbum)
-router.get('/albums', controllers.getAlbum)
-router.delete('/albums/:id', controllers.deleteAlbum)
+router.post('/api/albums', controllers.addAlbum)
+router.get('/api/albums', controllers.getAlbum)
+router.delete('/api/albums/:id', controllers.deleteAlbum)
 
 module.exports = router
