f3e981f5a5e91fa62989fe6cab6df8fcf57ede64
JYoung554 <jontefears10@gmail.com>
2022-10-03 15:29:51 -0400

    Updated

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/tranquil-reef-21193'
+    : 'mongodb://127.0.0.1:27017/todo_tracker'
 mongoose
   .connect(dbUrl, {
     useUnifiedTopology: true,
