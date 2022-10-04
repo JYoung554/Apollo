fac0907224bd128f85a12262b19ef845cdfef107
JYoung554 <jontefears10@gmail.com>
2022-10-03 23:01:47 -0400

    Updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const controllers = require('../controllers')
 const router = Router()
-//router.get('/', (req, res) => res.send('This is root!'))/
+router.get('/api', (req, res) => res.send('This is root!'))
 
 router.get('/api/artists', controllers.getArtists)
 router.post('/api/artists', controllers.addArtist)
